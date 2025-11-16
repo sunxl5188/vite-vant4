b3012e092e93d17d810946951bfa0bd94b961f84
364864264 <364864264@qq.com>
2025-11-16 20:56:12 +0800

    修改配置

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
   <van-config-provider>
-    <van-button-group>
-      <van-button type="primary" to="/">首页</van-button>
-      <van-button type="primary" to="/form">表单示例</van-button>
-    </van-button-group>
+    <van-button type="primary" to="/">首页</van-button>
+    <van-button type="primary" to="/form">表单示例</van-button>
     <router-view />
   </van-config-provider>
 </template>
